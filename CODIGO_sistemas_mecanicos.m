1da73e9399460e226d5a25e74eea69b7bda53ec9
marianovallejo5612 <mariano.vallejo5612@alumnos.udg.mx>
2026-02-08 14:36:25 -0600

    Add files via upload

---
+++ CODIGO_sistemas_mecanicos.m
@@ -0,0 +1,20 @@
+T = [0 10];
+x0 = zeros(8, 1); 
+[t, x] = ode45(@CARRO, T, x0);
+
+figure
+subplot(2,1,1)
+plot(t, x(:,1), t, x(:,3))
+title('z1')
+legend('Masa 1 (x1)', 'Masa 2 (x2)')
+grid on
+
+subplot(2,1,2)
+plot(t, x(:,5), t, x(:,7))
+title('z2')
+legend('Masa 1 (x1)', 'Masa 2 (x2)')
+grid on
+xlabel('Tiempo (s)')
+
+
+
